7665336c01c46ffe3c3b41c059c7dc7bc5d0ed3c
Jasmine <j.asra.harper@gmail.com>
2023-06-15 11:05:33 +0100

    made message text required to post

--- models/post.js
+++ models/post.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   author: String,
-  message: String,
+  message: {
+    type: String,
+    required: true
+  },
   likes: { type: Number, default: 0 },
   comments: Array
 });
